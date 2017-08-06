40f8f821b0f108d3d8a14f2ce602823f0bdd3351
winnie131212592 <winnie131212592@gmail.com>
2017-08-06 10:02:14 +0800

    index.php

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
-
+<?php
+  // Author: winnie damayo
+ ?>
 
 <!DOCTYPE html>
 <html >
   <head>
     <meta charset="UTF-8">
     <title>Pharmaceutical Inventory System for Regis Pharmacy</title>
-	
-    
+
+
         <link rel="stylesheet" href="assets/css/style.css">
 
-    
+
   </head>
 
   <body style="background: #262626;">
   <br/>
-  <br/>    
-    
-  
+  <br/>
+
+
 
     <div class="container">
 
@@ -27,9 +29,9 @@
     <fieldset>
 
       <form id="form-login">
-        <input type="text" autofocus id="un" placeholder="Username" required autocomplete="off"> 
+        <input type="text" autofocus id="un" placeholder="Username" required autocomplete="off">
 
-        <input type="password" id="up" placeholder="Password" required autocomplete="off">  
+        <input type="password" id="up" placeholder="Password" required autocomplete="off">
 
         <input type="submit" name="log" value="Login">
       </form>
@@ -39,17 +41,13 @@
   </div> <!-- end login-form -->
 
 </div>
-    
-    
-    
+
+
+
 <script type="text/javascript" src="assets/js/jquery-1.12.3.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="assets/js/regis.js"></script>
 
-    
+
   </body>
 </html>
-
-
-
- 
